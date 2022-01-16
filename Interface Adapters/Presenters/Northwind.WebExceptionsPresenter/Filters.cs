91a7795d37c2c473e83afc68569bf03b10944751
Elvis Nava <navaelvis@gmail.com>
2022-01-15 20:33:37 -0400

    UI

---
+++ Interface Adapters/Presenters/Northwind.WebExceptionsPresenter/Filters.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.WebExceptionsPresenter
+{
+    public static class Filters
+    {
+        public static void Register(MvcOptions options)
+        {
+            options.Filters.Add(new ApiExceptionFilterAttribute(
+                new Dictionary<Type, IExceptionHandler>
+                {
+                    { typeof(GeneralException), new GeneralExceptionHandler() },
+                    { typeof(ValidationException), new ValidationExceptionHandler() }
+                }
+            ));
+        }
+    }
+}
